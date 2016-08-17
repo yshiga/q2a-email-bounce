a8d3b450a5e7bb01c95295b48f97e47b13ed5fd1
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-08-17 14:37:16 +0900

    Add plugin file

---
+++ qa-plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+	Plugin Name: Email Bounce
+	Plugin URI:
+	Plugin Description: Notify the user of an unachievable mail.
+	Plugin Version: 1.0
+	Plugin Date: 2016-08-17
+	Plugin Author: 38qa.net
+	Plugin Author URI: http://www.question2answer.org/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+// process
+qa_register_plugin_module('process', 'qa-email-bounce-api.php', 'qa_email_bounce', 'Email Bounce API');
