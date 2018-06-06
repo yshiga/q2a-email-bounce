92f75fdfee45f46bba8f94d132a7db1dc86f2319
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-06-06 11:44:44 +0900

    プロフィール更新時emailbounceのフラグを更新

--- qa-email-bounce-event.php
+++ qa-email-bounce-event.php
@@ -9,9 +9,13 @@ class q2a_email_bounce_event {
     function process_event($event, $post_userid, $post_handle, $cookieid, $params)
     {
         if ($event === 'u_save') {
-            error_log('u_save: user_id: '.$post_userid);
-            $email=qa_get_user_email($post_userid);
-            error_log('u_save: email: '.$email);
+            $cur_email=qa_get_user_email($post_userid);
+            $bouncemail = email_bounce_db::get_email_from_userid($post_userid);
+            foreach ($bouncemail as $email) {
+                if ($cur_email !== $email) {
+                    email_bounce_db::update_emailbounce($post_userid, $email, 0);
+                }
+            }
         }
     }
 }
