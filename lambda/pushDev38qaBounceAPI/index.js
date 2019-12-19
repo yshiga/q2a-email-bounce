11783dffcae1689a7a6e5ef70bdbbe59ca4bb74e
yaimapp <gmonday730-mbp@yahoo.co.jp>
2019-12-19 09:31:39 +0900

    APIURLとAPIトークンを環境変数から取得

--- lambda/pushDev38qaBounceAPI/index.js
+++ lambda/pushDev38qaBounceAPI/index.js
@@ -5,21 +5,20 @@ exports.handler = function(event, context)
     var message = event.Records[0].Sns.Message;
     // Messageは文字列なので一度オブジェクトに変換
     var msg_obj = JSON.parse(message);
-
     if (msg_obj.notificationType == 'Bounce') {
       // bounceオブジェクトを文字列に変換
       var bounce = JSON.stringify(msg_obj.bounce);
       var headers = {
         'Content-Type':'text/plain'
-      }
+      };
       var options = {
-          url: 'https://dev.38qa.net/',
+          url: process.env.BASE_URL,
           method: 'POST',
           headers: headers,
           form: {
             'qa': 'ajax',
             'qa_operation': 'email_bounce',
-            'api_token': 'ySXFVr7pkrrFd*5oeg19i4AhvhxRSO',
+            'api_token': process.env.API_TOKEN,
             bounce: bounce,
           },
       };
